6aa77b755d9355f1a33f89fc77762157bdcb0ea1
Piotr Kaminski <piotr@ideanest.com>
2016-08-04 02:08:59 -0700

    Initial draft.

---
+++ fix_transaction.js
@@ -0,0 +1,62 @@
+(function() {
+  'use strict';
+
+  var originalTransaction = Firebase.prototype.transaction;
+
+  Firebase.ABORT_TRANSACTION_NOW = {};
+
+  Firebase.prototype.transaction = function transaction(updateFunction, onComplete, applyLocally) {
+    var aborted, tries = 0, ref = this, updateError;
+
+    var promise = new Promise(function(resolve, reject) {
+      var wrappedUpdate = function(data) {
+        aborted = false;
+        try {
+          if (++tries > 100) throw new Error('maxretry');
+          var result = updateFunction.call(this, data);
+          if (result === undefined) {
+            aborted = true;
+            result = data;
+          } else if (result === Firebase.ABORT_TRANSACTION_NOW) {
+            aborted = true;
+            result = undefined;
+          }
+          return result;
+        } catch (e) {
+          // Firebase propagates exceptions thrown by the update function to the top level.  So
+          // catch them here instead, reject the promise, and abort the transaction by returning
+          // undefined.
+          updateError = e;
+          reject(e);
+        }
+      };
+
+      function txn() {
+        try {
+          originalTransaction.call(ref, wrappedUpdate, function(error, committed, snapshot) {
+            error = error || updateError;
+            var result;
+            if (error && error.message === 'set') {
+              txn();
+            } else if (error) {
+              result = onComplete(error, false, snapshot);
+              reject(error);
+            } else {
+              result = onComplete(error, committed && !aborted, snapshot);
+              resolve({committed: committed && !aborted, snapshot: snapshot});
+            }
+            return result;
+          }, applyLocally);
+        } catch (e) {
+          onComplete(e, false);
+          reject(e);
+        }
+      }
+
+      txn();
+    });
+
+    return promise;
+
+  };
+})();
